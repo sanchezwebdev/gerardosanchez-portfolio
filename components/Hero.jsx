022acc5be873981fe554b5468264ca0350583845
Gerardo <sanchezwebdev@gmail.com>
2025-07-04 11:29:16 -0500

    image optimization

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import styles from '../styles/Hero.module.css'
+import React from "react";
+import styles from "../styles/Hero.module.css";
+import Head from 'next/head'
 
 const Hero = () => {
   return (
-    <div className={styles.container}>
+    <>
+      <Head>
+        <link rel="preload" as="image" href="/face.png" />
+      </Head>
+      <div className={styles.container}>
         <h1 className={styles.header}>Full-Stack Developer</h1>
-        <h2 className={styles.subheader}>I develop web applications & make YouTube videos, and I love what I do.</h2>
+        <h2 className={styles.subheader}>
+          I develop web applications & make YouTube videos, and I love what I
+          do.
+        </h2>
         <div className={styles.imageWrapper}>
-            <img src="/face.png" alt="" className={styles.image}/>
+          <img src="/face.png" alt="" className={styles.image} />
         </div>
-        </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Hero
-
+export default Hero;
