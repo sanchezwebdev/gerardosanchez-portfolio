b46cbb29586a90770e466795b23816d2e2b301e1
Gerardo <sanchezwebdev@gmail.com>
2025-07-04 12:55:48 -0500

    updated hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -11,8 +11,7 @@ const Hero = () => {
       <div className={styles.container}>
         <h1 className={styles.header}>Full-Stack Developer</h1>
         <h2 className={styles.subheader}>
-          I develop web applications & make YouTube videos, and I love what I
-          do.
+          I develop full-stack web applications, and I love what I do.
         </h2>
         <div className={styles.imageWrapper}>
           <img src="/face.png" alt="" className={styles.image} />
